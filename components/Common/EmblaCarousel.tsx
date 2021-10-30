75aa8913ce39d81edc8fbd58bf19822ef463c8eb
Daniel Galán <47415745+danngalann@users.noreply.github.com>
2021-10-30 20:10:48 +0200

    Less padding. Lazy load images.

--- components/Common/EmblaCarousel.tsx
+++ components/Common/EmblaCarousel.tsx
@@ -48,6 +48,7 @@ const EmblaCarousel = ({
                   className={styles.embla_slid_img}
                   src={mediaByIndex(index).src}
                   alt=""
+                  loading="lazy"
                 />
               </div>
             </div>
