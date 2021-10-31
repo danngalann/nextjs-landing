366ed5629587cdf8c0f09439112a8b9c0918345b
Daniel Galán <47415745+danngalann@users.noreply.github.com>
2021-10-31 12:11:39 +0100

    Contain CSS within component

--- components/Common/EmblaCarousel.tsx
+++ components/Common/EmblaCarousel.tsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect, useCallback } from "react";
 import useEmblaCarousel from "embla-carousel-react";
-import styles from "../../styles/Home.module.css";
-import { Image } from "@chakra-ui/react";
+import { Image, chakra, Box } from "@chakra-ui/react";
 
 const EmblaCarousel = ({
   slides,
@@ -38,24 +37,31 @@ const EmblaCarousel = ({
   }, [embla, onSelect, scrollNext]);
 
   return (
-    <div className={styles.embla}>
-      <div className={styles.embla__viewport} ref={viewportRef}>
-        <div className={styles.embla__container}>
+    <chakra.div position="relative" maxWidth="100vw" margin="0 4vw">
+      <chakra.div overflow="hidden" width="100%" ref={viewportRef}>
+        <chakra.div display="flex" userSelect="none" marginLeft="-10px">
           {slides.map((index: number) => (
-            <div className={styles.embla__slide} key={index}>
-              <div className={styles.embla__slide__inner}>
+            <chakra.div
+              position="relative"
+              minWidth="100%"
+              paddingLeft="10px"
+              key={index}
+            >
+              <chakra.div position="relative" overflow="hidden" maxHeight="80vh">
                 <Image
-                  className={styles.embla_slid_img}
                   src={mediaByIndex(index).src}
+                  // className={styles.embla__slide__img}
                   alt=""
                   layout="responsive"
+                  height="auto"
+                  width="100%"
                 />
-              </div>
-            </div>
+              </chakra.div>
+            </chakra.div>
           ))}
-        </div>
-      </div>
-    </div>
+        </chakra.div>
+      </chakra.div>
+    </chakra.div>
   );
 };
 
