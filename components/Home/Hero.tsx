f8f85da9f9afba4b97f3b387d07a63648433d9ea
Daniel Galán <47415745+danngalann@users.noreply.github.com>
2021-11-06 12:04:31 +0100

    Multilayer parallax 'cause it looks dope

--- components/Home/Hero.tsx
+++ components/Home/Hero.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Image from "next/image";
 import { Box, chakra } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
+import { ParallaxBanner } from "react-scroll-parallax";
 
 const Snowfall = dynamic(
   () => {
@@ -15,14 +16,36 @@ export default function Hero() {
     <chakra.div>
       <Snowfall color="#E3F7FA" snowflakeCount={200} style={{ zIndex: 10 }} />
 
-      <Box position="relative" w="full" height="80vh">
+      <ParallaxBanner
+        layers={[
+          {
+            image: "img/hero/back_faded.png",
+            amount: 0.4,
+            expanded: false,
+          },
+          {
+            image: "img/hero/middle.png",
+            amount: 0.3,
+            expanded: false,
+          },
+          {
+            image: "img/hero/front.png",
+            amount: 0.1,
+            expanded: false,
+          },
+        ]}
+        style={{
+          height: "60rem",
+        }}
+      ></ParallaxBanner>
+      {/* <Box position="relative" w="full" height="80vh">
         <Image
           src="/img/hero/cold_mountain.jpg"
           alt="cold mountain"
           layout="fill"
           objectFit="cover"
         />
-      </Box>
+      </Box> */}
     </chakra.div>
   );
 }
