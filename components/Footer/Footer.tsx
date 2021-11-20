6b1fafab9da8644313d8fe356a600c5a6a7d3990
Daniel Galán <47415745+danngalann@users.noreply.github.com>
2021-11-20 12:06:24 +0100

    Responsive footer

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -1,13 +1,4 @@
-import {
-  Box,
-  Container,
-  Stack,
-  Text,
-  Flex,
-  Link,
-  Tag,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import { Box, Container, Stack, Text, Flex, Link, Tag } from "@chakra-ui/react";
 import { ReactNode } from "react";
 import Logo from "../Common/Logo";
 import NextLink from "next/link";
@@ -22,82 +13,70 @@ const ListHeader = ({ children }: { children: ReactNode }) => {
 
 export default function Footer() {
   return (
-    <Box
-      bg={useColorModeValue("gray.100", "gray.900")}
-      color={useColorModeValue("gray.700", "gray.200")}
-    >
-      <Container as={Stack} maxW={"6xl"} py={10}>
-        <Flex
-          justifyContent="space-around"
-          direction={{ sm: "column", md: "row" }}
-        >
-          <Stack align={"flex-start"}>
-            <ListHeader>Productos</ListHeader>
-            <Link href="http://shop.inserte.com/shop/">Todo</Link>
-            <Link href="http://shop.inserte.com/product-category/productos-de-mar/">
-              Pescado
-            </Link>
-            <Flex>
-              <Text color="gray.400">Carnes</Text>
-              <Tag
-                size={"sm"}
-                bg="green.300"
-                ml={2}
-                color={"white"}
-              >
-                Próximamente
-              </Tag>
-            </Flex>
-            <Flex>
-              <Text color="gray.400">Postres</Text>
-              <Tag
-                size={"sm"}
-                bg="green.300"
-                ml={2}
-                color={"white"}
-              >
-                Próximamente
-              </Tag>
-            </Flex>
-          </Stack>
-          <Stack align={"flex-start"}>
-            <ListHeader>Empresa</ListHeader>
-            <NextLink href="/" passHref>
-              <Link>Inicio</Link>
-            </NextLink>
-            <NextLink href="/empresa" passHref>
-              <Link>Sobre nosotros</Link>
-            </NextLink>
-            <NextLink href="/contacto" passHref>
-              <Link>Contacto</Link>
-            </NextLink>
-          </Stack>
-          <Stack align={"flex-start"}>
-            <ListHeader>Legal</ListHeader>
-            <NextLink href="/legal/cookies" passHref>
-              <Link>Política de Cookies</Link>
-            </NextLink>
-            <Link>Privacy Policy</Link>
-            <Link>Terms of Service</Link>
-            <Link>Law Enforcement</Link>
-            <Link>Status</Link>
-          </Stack>
-        </Flex>
-      </Container>
+    <Box bg="gray.100" color="gary.700">
+      <Flex
+        justifyContent="space-around"
+        direction={{ base: "column", md: "row" }}
+        maxW={{ base: "full", md: "6xl" }}
+        py={{base: 5, md: 10}}
+        margin="0 auto"
+      >
+        <Stack alignItems={{base: "center", md:"flex-start"}} my={{base: 4, md: 0}}>
+          <ListHeader>Productos</ListHeader>
+          <Link href="http://shop.inserte.com/shop/">Todo</Link>
+          <Link href="http://shop.inserte.com/product-category/productos-de-mar/">
+            Pescado
+          </Link>
+          <Flex>
+            <Text color="gray.400">Carnes</Text>
+            <Tag size={"sm"} bg="green.300" ml={2} color={"white"}>
+              Próximamente
+            </Tag>
+          </Flex>
+          <Flex>
+            <Text color="gray.400">Postres</Text>
+            <Tag size={"sm"} bg="green.300" ml={2} color={"white"}>
+              Próximamente
+            </Tag>
+          </Flex>
+        </Stack>
+        <Stack alignItems={{base: "center", md:"flex-start"}} my={{base: 4, md: 0}}>
+          <ListHeader>Empresa</ListHeader>
+          <NextLink href="/" passHref>
+            <Link>Inicio</Link>
+          </NextLink>
+          <NextLink href="/empresa" passHref>
+            <Link>Sobre nosotros</Link>
+          </NextLink>
+          <NextLink href="/contacto" passHref>
+            <Link>Contacto</Link>
+          </NextLink>
+        </Stack>
+        <Stack alignItems={{base: "center", md:"flex-start"}} my={{base: 4, md: 0}}>
+          <ListHeader>Legal</ListHeader>
+          <NextLink href="/legal/cookies" passHref>
+            <Link>Política de Cookies</Link>
+          </NextLink>
+          <Link>Privacy Policy</Link>
+          <Link>Terms of Service</Link>
+          <Link>Law Enforcement</Link>
+          <Link>Status</Link>
+        </Stack>
+      </Flex>
       <Box py={10}>
         <Flex
           align={"center"}
           _before={{
             content: '""',
             borderBottom: "1px solid",
-            borderColor: useColorModeValue("gray.200", "gray.700"),
+            borderColor: "gray.200",
             flexGrow: 1,
             mr: 8,
           }}
           _after={{
             content: '""',
             borderBottom: "1px solid",
-            borderColor: useColorModeValue("gray.200", "gray.700"),
+            borderColor: "gray.200",
             flexGrow: 1,
             ml: 8,
           }}
